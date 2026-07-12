bbf62d1f615395e6b2b61d7c47d0e0d1ca798342
dharmik-rabari-03 <dharmik161616@gmail.com>
2026-07-12 19:26:36 +0530

    some changes

---
+++ practice/intial-Setup/server.js
@@ -0,0 +1,68 @@
+import express from "express"
+import httpError from "./middleware/httpError.js"
+
+
+
+
+const app = express()
+
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+
+    res.send("hello from server")
+
+})
+
+
+app.use((req, res) => {
+    res.status(404)
+        .json({ success: true, message: "requested route not found" })
+})
+
+app.use((Error, req, res, next) => {
+
+    if (res.headersSent) {
+
+
+        return next(error)
+    }
+
+    res.status(Error.statusCode || 500)
+        .json({ message: Error.message || "something went wrong" })
+
+})
+
+const port = 5000;
+
+
+
+async function server() {
+
+    try {
+
+        const connect = await dbConnect()
+
+        if (!connect) {
+            throw new Error("failed to connect DB")
+        }
+
+        app.listen(port, (err) => {
+            if (err) {
+                return console.log(err.message)
+            }
+
+            console.log(`server is running on port ${port}`)
+        })
+
+
+    } catch (error) {
+        console.log(error.message)
+
+        process.exit(1)
+    }
+
+}
+
+server()
