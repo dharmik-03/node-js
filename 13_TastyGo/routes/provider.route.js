863240a4ce97b5f3c65388a23aa1a45774836ac0
dharmik-rabari-03 <dharmik161616@gmail.com>
2026-08-04 11:08:42 +0530

    some changes

--- 13_TastyGo/routes/provider.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import providerController from "../controller/provider.Controller.js";
-import auth from "../middlewares/auth.js"
-
-const route = express.Router()
-
-route.post("/add", auth, providerController.addProvider)
-
-export default route
